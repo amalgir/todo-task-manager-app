213bb401ecd7c8482a6ab272f70a9115db08abe0
amalgir <101013202+amalgir@users.noreply.github.com>
2022-03-13 14:41:46 +0530

    feat: add edittext to input tasks and recyclerView to display task list

--- app/src/main/java/com/example/todo_task_manager/NewTaskActivity.java
+++ app/src/main/java/com/example/todo_task_manager/NewTaskActivity.java
@@ -3,22 +3,27 @@ package com.example.todo_task_manager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class NewTaskActivity extends AppCompatActivity {
     private TextView newCategoryTextView;
     private EditText newCategoryEditText;
+    private EditText taskEditText;
     private boolean editVisible = false;
-
+    private ArrayList<TaskCategory> taskList;
+    private RecyclerView taskRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,8 @@ public class NewTaskActivity extends AppCompatActivity {
 
         newCategoryEditText = findViewById(R.id.taskCategoryEditText);
         newCategoryTextView = findViewById(R.id.taskCategoryTextView);
+        taskEditText = findViewById(R.id.editTextTaskList);
+        taskRecyclerView = findViewById(R.id.TaskListRecyclerView);
 
         initializeElementVisibility();
     }
@@ -54,15 +61,21 @@ public class NewTaskActivity extends AppCompatActivity {
         int itemId = item.getItemId();
         if (itemId == R.id.editMenu){
             newCategoryEditText.setVisibility(View.VISIBLE);
+            taskEditText.setVisibility(View.VISIBLE);
             newCategoryTextView.setVisibility(View.GONE);
+            taskRecyclerView.setVisibility(View.GONE);
             editVisible = false;
             invalidateOptionsMenu();
         }
         else if (itemId == R.id.saveMenu){
             newCategoryEditText.setVisibility(View.GONE);
+            taskEditText.setVisibility(View.GONE);
             newCategoryTextView.setVisibility(View.VISIBLE);
+            taskRecyclerView.setVisibility(View.VISIBLE);
             editVisible = true;
+            saveTasks();
             invalidateOptionsMenu();
+            setAdaptor();
         }
         return true;
     }
@@ -71,4 +84,39 @@ public class NewTaskActivity extends AppCompatActivity {
         newCategoryEditText.setVisibility(View.VISIBLE);
         newCategoryTextView.setVisibility(View.GONE);
     }
+
+    public void saveTasks(){
+        // Getting tasks entered by user
+        String taskCategoryText = newCategoryEditText.getText().toString();
+        String taskText = taskEditText.getText().toString();
+        // Setting text in textView
+        System.out.println(taskCategoryText);
+        System.out.println(taskText);
+        newCategoryTextView.setText(taskCategoryText);
+        // Setting tasks in recycler view
+        taskList = new ArrayList<>();
+        String[] taskArray =  taskText.split("\n",20);
+        setTaskInfo(taskArray);
+
+    }
+
+    private void setAdaptor() {
+        recyclerAdapter adapter = new recyclerAdapter(taskList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        taskRecyclerView.setLayoutManager(layoutManager);
+        taskRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        taskRecyclerView.setAdapter(adapter);
+    }
+
+    private void setTaskInfo(String[] stringArray){
+        if (stringArray.length > 0){
+            for (String s : stringArray) {
+                taskList.add(new TaskCategory('\u23FA' + "    " + s));
+            }
+        }
+    }
+
+
+
+
 }
